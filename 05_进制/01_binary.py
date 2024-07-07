85a160c735cc5b02a56b9e8ac25a39285be43556
tobysieam <tobysieam@hotmail.com>
2024-07-07 20:45:28 +0800

    2024.7.7

---
+++ 05_进制/01_binary.py
@@ -0,0 +1,30 @@
+# @Version  : 1.0
+# @Author   : Cheng Huang
+# @File     :01_binary.py
+# @Time     :2024/7/7 下午7:50
+
+# 2进制
+print(0b111)
+print(0B111)
+# 8进制
+print(0o111)
+print(0O111)
+# 十进制
+print(111)
+# 16进制
+print(0x111)
+print(0X111)
+# 转换
+print("----------")
+print(0b1011)
+print(0o234)
+print(0x23A)
+
+
+print("----------")
+print(bin(34))
+print(oct(131))
+print(hex(237))
+print(bin(123))
+print(oct(678))
+print(hex(8912))
